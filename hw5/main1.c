5591e5bb9237a6652fac55d79bdab5b6ae4c631d
Sayhello_bro <144075540+Sayhellobro@users.noreply.github.com>
2023-12-19 23:56:32 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,13 +1,17 @@
+#include <stdio.h>
+
 int main()
 {
     int i,j;
+    
     for(i=1;i<=7;i++){
-        for(j=1;j<=7-i;j++){
-            printf(" ");
-        }
-        for(j=1;j<=i;j++){
-            printf("%d ",i);
+        
+        printf("%*d",8-i,i);
+        for(j=1;j<=i-1;j++){
+            printf("%*d",2,i);
         }
         printf("\n");
     }
+    
+    return 0;
 }
